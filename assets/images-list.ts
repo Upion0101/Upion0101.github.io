2e7d7d9c0ad48c228dbf9404187170b59599aba8
Abel Al-Hussainawi <asalhu01@louisville.edu>
2023-10-29 19:29:55 -0400

    Auto-generated commit [ci skip]

--- assets/images-list.ts
+++ assets/images-list.ts
@@ -1,1 +1,50 @@
-export const IMAGES_LIST = ["IMG_4482.png","IMG_4509.JPG","IMG_4553.png","IMG_4558.png","IMG_4575.png","IMG_4669.png","IMG_4675.png","IMG_4676.png","IMG_4688.png"];
+// images-list.ts
+
+export const IMAGES_LIST = [
+    {
+      filename: "IMG_4482.png",
+      caption: "Denzel Washington Bob",
+      isZoomed: false
+    },
+    {
+      filename: "IMG_4509.JPG",
+      caption: "Bob's Casual Adventure Day",
+      isZoomed: false
+    },
+    {
+      filename: "IMG_4553.png",
+      caption: "Grandma Bob",
+      isZoomed: false
+    },
+    {
+      filename: "IMG_4558.png",
+      caption: "Bob Takes a Sip From Mama's Water Cup",
+      isZoomed: false
+    },
+    {
+      filename: "IMG_4575.png",
+      caption: "Wow, She Can Yawn!",
+      isZoomed: false
+    },
+    {
+      filename: "IMG_4669.png",
+      caption: "Bob and the Cardboard Box Retreat",
+      isZoomed: false
+    },
+    {
+      filename: "IMG_4675.png",
+      caption: "Bob's Sunshine Spot by the Window",
+      isZoomed: false
+    },
+    {
+      filename: "IMG_4676.png",
+      caption: "Guess Which One is Bob",
+      isZoomed: false
+    },
+    {
+      filename: "IMG_4688.png",
+      caption: "Bob's Unconventional Nap Position",
+      isZoomed: false
+    }
+  ];
+  
